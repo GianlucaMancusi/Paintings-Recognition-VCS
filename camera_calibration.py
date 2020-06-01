450f4ceb630949979b19616beb8bb10649d6480a
vittoriopippi <219424@studenti.unimore.it>
2020-06-01 10:45:57 +0200

    Edit auto camera calibration

--- camera_calibration.py
+++ camera_calibration.py
@@ -1,62 +1,27 @@
 import cv2
 import matplotlib.pyplot as plt
 from step_09_hough import hough
-from step_08_canny_edge_detection import apply_edge_detection
-
+from step_08_canny_edge_detection import _apply_edge_detection
+from skimage.transform import hough_line
 import numpy as np
-
-k_min = 0
-k_max = 1
-n = 20
-
-
-def undistort(img):
-    pass
-
-
-# QUI STAVO IMPLEMENTANDO IL PAPER DELLA CUCCHIARA...
-# def findKValue(img):
-#     original_ROI = img[:365, :100]
-#     c_x, c_y = img.shape[0]//2, img.shape[1]//2
-
-#     new_ROI = np.zeros_like(original_ROI)
-#     index_matrix = np.indices(new_ROI.shape)
-#     index_matrix[0] = index_matrix[0]-c_x
-#     index_matrix[1] = index_matrix[1]-c_y
-
-#     for i in range(n):
-#         k1 = k_min + i * (k_max - k_min)/n
-#         # distorted radius
-#         r_d = np.sqrt(index_matrix[0]**2+index_matrix[1]**2)
-#         # undistorted coords (new image)
-#         x_u = index_matrix[0] + (index_matrix[0] - c_x)*(k1*r_d**2)
-#         y_u = index_matrix[1] + (index_matrix[1] - c_y)*(k1*r_d**2)
-#         # undistorted radius
-#         r_u = np.sqrt(index_matrix[0]**2+index_matrix[1]**2)
-#         if k1 > 0:
-#             r_d = np.cbrt(r_u/(2*k1)+np.sqrt((1/(3*k1))**3+(r_u/(2*k1))**2)) + \
-#                 np.cbrt(r_u/(2*k1)-np.sqrt((1/(3*k1))**3+(r_u/(2*k1))**2))
-#         # distorted coords (original image)
-#         x_d = c_x + (index_matrix[0]-c_x)*(r_d/r_u)
-#         y_d = c_y + (index_matrix[1]-c_y)*(r_d/r_u)
-
-#         new_ROI = original_ROI[y_d, x_d]
-
-#     # LOOP algorithm
+from matplotlib import cm
+from matplotlib.ticker import LinearLocator, FormatStrFormatter
+from generate_painting_masks import generate_mask
+from scipy.ndimage import gaussian_filter1d
+from step_03_dilate_invert import _add_padding
 
 
 def nothing(x):
     pass
 
 
-def fisheye(img, k1=0, k2=0, fx=1230, fy=1230):
-
+def _undistort(img, k1=0, k2=0, fx=857, fy=876):
     img = img.copy()
     # K = np.array([[5.00*img.shape[1],     0.,  img.shape[1]//2],
     #              [0.,   5.00*img.shape[0],   img.shape[0]//2],
     #              [0.,     0.,     1.]])
-    K = np.array([[fx,     0.,  img.shape[1]//2],
-                  [0.,   fy,   img.shape[0]//2],
+    K = np.array([[fx,     0.,     img.shape[1]//2],
+                  [0.,     fy,     img.shape[0]//2],
                   [0.,     0.,     1.]])
 
     # zero distortion coefficients work well for this image
@@ -66,31 +31,52 @@ def fisheye(img, k1=0, k2=0, fx=1230, fy=1230):
     Knew = K.copy()
     Knew[(0, 1), (0, 1)] = .6 * Knew[(0, 1), (0, 1)]
     img_undistorted = cv2.undistort(img, K, D, newCameraMatrix=Knew)
-    #img_undistorted = cv2.fisheye.undistortImage(img, K, D=D, Knew=Knew)
     return img_undistorted
+
+
+def undistort(img, **kwargs):
+    out = _undistort(img, **kwargs)
+    # kwargs['k1'] = 0
+    # kwargs['k2'] = 0
+    # s1, e1, s2, e2 = calculate_crop_spot(_undistort(img, **kwargs))
+    # out = out[s2:e2, s1:e1]
+    out = without_black_pad(out)
+    out = cv2.resize(out, (img.shape[1], img.shape[0]))
+    return out
+
+
+def threshold_undistort(img, crop_spot=None, **kwargs):
+    out = _undistort(img, **kwargs)
+    if crop_spot:
+        s1, e1, s2, e2 = crop_spot
+        out = out[s2:e2, s1:e1]
+    return out
 
 
 def apply_fisheye(img, debug=False):
     base_img = img.copy()
 
     if not debug:
-        img_undistorted = fisheye(base_img, k1=-0.38, k2=0.106, fx=872, fy=872)
+        img_undistorted = undistort(base_img, k1=-0.38, k2=0.106, fx=872, fy=872)
     else:
         cv2.namedWindow('undistorted')
-        cv2.createTrackbar('k1', 'undistorted', 0, 1000, nothing)
-        cv2.createTrackbar('k2', 'undistorted', 0, 1000, nothing)
-        cv2.createTrackbar('fx', 'undistorted', 0, 5500, nothing)
+        cv2.createTrackbar('k1', 'undistorted', 0, 10000, nothing)
+        # cv2.createTrackbar('k2', 'undistorted', 0, 1000, nothing)
+        # cv2.createTrackbar('fx', 'undistorted', 0, 5500, nothing)
         cv2.createTrackbar('fy', 'undistorted', 0, 5500, nothing)
         while(1):
             # get current positions of four trackbars
             k1 = cv2.getTrackbarPos('k1', 'undistorted')
-            k2 = cv2.getTrackbarPos('k2', 'undistorted')
-            fx = cv2.getTrackbarPos('fx', 'undistorted')
+            # k2 = cv2.getTrackbarPos('k2', 'undistorted')
+            # fx = cv2.getTrackbarPos('fx', 'undistorted')
             fy = cv2.getTrackbarPos('fy', 'undistorted')
-            k1 = k1/1000*2-.5
-            k2 = k2/1000*2-.5
+            k1 = k1/10000*20 - 10
+            k2 = k1
+            fx = fy
 
-            img_undistorted = fisheye(base_img, k1=k1, k2=k2, fx=fx, fy=fy)
+            print(k1, fx)
+
+            img_undistorted = undistort(base_img, k1=k1, k2=k2, fx=fx, fy=fy)
             cv2.imshow('undistorted', img_undistorted)
             k = cv2.waitKey(1) & 0xFF
             if k == 27:
@@ -98,17 +84,141 @@ def apply_fisheye(img, debug=False):
         cv2.destroyAllWindows()
     return img_undistorted
 
+def vertical_lines(half_freedom=1):
+    angles = np.arange(0, np.pi, np.pi/180)
+    a = angles[:half_freedom]
+    b = angles[-half_freedom:]
+    return np.concatenate((a, b))
+
+def horizontal_lines(half_freedom=1):
+    angles = np.arange(0, np.pi, np.pi/180)
+    return angles[90 - half_freedom: 90 + half_freedom]
+
+def hv_lines(half_freedom=1):
+    return np.concatenate((vertical_lines(half_freedom), horizontal_lines(half_freedom)))
+
+def calculate_HT(img, sigma=3):
+    angles = hv_lines(10)
+    H, _, _ = hough_line(img, angles)
+    H = gaussian_filter1d(H, sigma)
+    return H.max()
+
+def HTRDC(img, steps=50, range_min=-0.25, range_max=0, epsilon=0.0001, iteration=0, prev_k=None):
+    step = (range_max - range_min)/steps
+    K_range = np.arange(range_min, range_max, step)
+    HT_vals = np.zeros_like(K_range)
+    HT_vals_focus = np.zeros_like(K_range)
+    crop_spot = calculate_crop_spot(threshold_undistort(img))
+    for i, k in enumerate(K_range):
+        img_undistorted = threshold_undistort(img, crop_spot=crop_spot, k1=k)
+        HT = calculate_HT(img_undistorted)
+        HT_vals_focus[i] = HT
+        img_undistorted = threshold_undistort(img, k1=k)
+        HT = calculate_HT(img_undistorted)
+        HT_vals[i] = HT
+        cv2.imshow('undistorted', img_undistorted)
+        cv2.waitKey(1)
+        print(f'k={k:.08f} HT={HT}')
+
+    smoothed_HT_vals = gaussian_filter1d(HT_vals, 1.5)
+    # plt.plot(K_range, HT_vals, label='HT')
+    # plt.plot(K_range, HT_vals_focus, label='HT_focused')
+    # plt.legend()
+    # # plt.plot(K_range, smoothed_HT_vals)
+    # plt.show()
+    print(f'k={K_range[HT_vals.argmax()]} k_smooth={K_range[smoothed_HT_vals.argmax()]}')
+    max_k = K_range[HT_vals_focus.argmax()]
+    if iteration != 0 and (prev_k - max_k) <= epsilon:
+        return max_k
+    else:
+        range_min = max(range_min, max_k - step)
+        range_max = min(max_k + step, range_max)
+        return HTRDC(img, steps, range_min, range_max, epsilon, iteration + 1, max_k)
+
+
+def distort_random(img):
+    return undistort(img, k1=-0.5, k2=-0.5)
+
+def without_black_pad(img):
+    s1, e1, s2, e2 = calculate_crop_spot(img)
+    return img[s2:e2, s1:e1]
+
+def calculate_crop_spot(img):
+    img = img.copy()
+    if img.ndim == 3:
+        img = img.sum(axis=2)
+    assert img.ndim == 2
+    width_max = img.max(axis=0)
+    height_max = img.max(axis=1)
+    s1, e1 = args_start_end_sim(width_max)
+    s2, e2 = args_start_end_sim(height_max)
+    return (s1, e1, s2, e2)
+
+def args_start_end(arr):
+    assert arr.ndim == 1
+    start = start_at(arr)
+    end = arr.shape[0] - start_at(arr[::-1])
+    return start, end
+
+def args_start_end_sim(arr):
+    assert arr.ndim == 1
+    start = start_at(arr)
+    end = start_at(arr[::-1])
+    res = min(start, end)
+    return res, arr.shape[0] - res
+
+def start_at(arr):
+    assert arr.ndim == 1
+    for i, val in enumerate(arr):
+        if val != 0:
+            break
+    return i
+
 
 if __name__ == "__main__":
-    img = cv2.imread("data_test\\fisheye.jpg", cv2.IMREAD_GRAYSCALE)
-    img = cv2.resize(img, (1280, 720))
-    cv2.imshow("normal", img)
-    # img = cv2.imread("data_test\\000090.jpg", cv2.IMREAD_GRAYSCALE)
-    #canny = apply_edge_detection(img)
-    #cv2.imshow("Canny", canny)
-    # cv2.waitKey()
-    # findKValue(canny)
-    #(himg, _), canvas = hough(canny, debug=True)
-    canvas = apply_fisheye(img, debug=False)
-    cv2.imshow("canvas", canvas)
-    cv2.waitKey()
+    from image_viewer import show_me, ImageViewer
+    from data_test.standard_samples import RANDOM_PAINTING, FISH_EYE, CHESSBOARD, TEST_DISTORTION, get_random_paintings
+    for paint in TEST_DISTORTION:
+        img = cv2.imread(paint)
+        img = cv2.resize(img, (1280, 720))
+        # img = distort_random(img)
+        iv = ImageViewer()
+        # img = cv2.imread("data_test\\000090.jpg", cv2.IMREAD_GRAYSCALE)
+        # for e in range(0,100,5):
+        mask = generate_mask(img)
+        iv.add(mask, cmap='bgr', title='mask')
+        
+        canvas = np.empty_like(mask, dtype=np.float32)
+        # canvas += _apply_edge_detection(mask, 150, 250)
+        canvas += _apply_edge_detection(img, 150, 250)
+        canvas = np.clip(canvas, 0, 255)
+        iv.add(img, cmap='bgr', title='original')
+        iv.add(canvas, cmap='bgr', title='canvas')
+
+        # apply_fisheye(img, True)
+
+        # vals_1 = []
+        # vals_2 = []
+        # pads = range(0, 500, 5)
+        # for pad in pads:
+        #     tmp = canny.copy()
+        #     tmp = _add_padding(tmp, pad)
+        #     # tmp = cv2.resize(tmp, (1280, 720))
+        #     HT = calculate_HT(tmp)
+        #     vals_1.append(HT)
+        #     tmp = without_black_pad(tmp)
+        #     HT = calculate_HT(tmp)
+        #     vals_2.append(HT)
+        #     cv2.imshow('tmp', tmp)
+        #     cv2.waitKey(1)
+        #     print(f'pad={pad} HT={vals_1[-1]} R_HT={vals_2[-1]}')
+        # plt.plot(pads, vals_1)
+        # plt.plot(pads, vals_2)
+        # plt.show()
+
+        k = HTRDC(canvas, steps=50, range_min=-0.25, range_max=0)
+        canvas = undistort(img, k1=k)
+        # canvas = undistort(img, k1=-0.257, k2=0.008)
+        iv.add(canvas, cmap='bgr', title=f'{paint} k={k:.06f}')
+        iv.show()
+        # show_me(canvas)
