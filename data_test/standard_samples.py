a95458f39be4705681001e7e3c1b53bdcb2d7149
vittoriopippi <219424@studenti.unimore.it>
2020-06-25 18:45:49 +0200

    Update samples

--- data_test/standard_samples.py
+++ data_test/standard_samples.py
@@ -1,22 +1,21 @@
-from os import listdir, walk
-from os.path import isfile, join, splitext
+import os
 import random
 
 dir_path = 'data_test/paintings'
-TEST_PAINTINGS = [join(dir_path, f).replace('\\', '/') for f in listdir(dir_path) if isfile(join(dir_path, f))]
+TEST_PAINTINGS = [os.path.join(dir_path, f).replace('\\', '/') for f in os.listdir(dir_path) if os.path.isfile(os.path.join(dir_path, f))]
 
 dir_path = 'data_test/paintings_retrieval'
-TEST_RETRIEVAL = [join(dir_path, f).replace('\\', '/') for f in listdir(dir_path) if isfile(join(dir_path, f))]
+TEST_RETRIEVAL = [os.path.join(dir_path, f).replace('\\', '/') for f in os.listdir(dir_path) if os.path.isfile(os.path.join(dir_path, f))]
 
 dir_path = 'data_test/camera_correction'
-TEST_DISTORTION = [join(dir_path, f).replace('\\', '/') for f in listdir(dir_path) if isfile(join(dir_path, f))]
+TEST_DISTORTION = [os.path.join(dir_path, f).replace('\\', '/') for f in os.listdir(dir_path) if os.path.isfile(os.path.join(dir_path, f))]
 
 dir_path = 'dataset/paintings_db'
-PAINTINGS_DB = [join(dir_path, f).replace('\\', '/') for f in listdir(dir_path) if isfile(join(dir_path, f))]
+PAINTINGS_DB = [os.path.join(dir_path, f).replace('\\', '/') for f in os.listdir(dir_path) if os.path.isfile(os.path.join(dir_path, f))]
 
 try:
     path = 'dataset/photos'
-    ALL_PAINTINGS = [join(dp, f) for dp, dn, filenames in walk(path) for f in filenames if splitext(f)[1] == '.jpg']
+    ALL_PAINTINGS = [os.path.join(dp, f) for dp, dn, filenames in os.walk(path) for f in filenames if os.path.splitext(f)[1] == '.jpg']
 except:
     pass
 
@@ -31,7 +30,7 @@ def get_random_paintings(number):
     return random.sample(ALL_PAINTINGS, number)
 
 def all_files_in(dir_path):
-    return [join(dir_path, f).replace('\\', '/') for f in listdir(dir_path) if isfile(join(dir_path, f))]
+    return [os.path.join(dir_path, f).replace('\\', '/') for f in os.listdir(dir_path) if os.path.isfile(os.path.join(dir_path, f))]
 
 if __name__ == "__main__":
     # print(TEST_PAINTINGS)
