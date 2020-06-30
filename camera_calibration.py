fb8a19934be604d39c8cba4a50c506536f582bf4
Platinum2d <manicardi215@gmail.com>
2020-06-30 11:39:26 +0200

    Comments removed

--- camera_calibration.py
+++ camera_calibration.py
@@ -17,9 +17,6 @@ def nothing(x):
 
 def _undistort(img, k1=0, k2=0, fx=857, fy=876):
     img = img.copy()
-    # K = np.array([[5.00*img.shape[1],     0.,  img.shape[1]//2],
-    #              [0.,   5.00*img.shape[0],   img.shape[0]//2],
-    #              [0.,     0.,     1.]])
     K = np.array([[fx,     0.,     img.shape[1]//2],
                   [0.,     fy,     img.shape[0]//2],
                   [0.,     0.,     1.]])
@@ -36,10 +33,6 @@ def _undistort(img, k1=0, k2=0, fx=857, fy=876):
 
 def undistort(img, **kwargs):
     out = _undistort(img, **kwargs)
-    # kwargs['k1'] = 0
-    # kwargs['k2'] = 0
-    # s1, e1, s2, e2 = calculate_crop_spot(_undistort(img, **kwargs))
-    # out = out[s2:e2, s1:e1]
     out = without_black_pad(out)
     out = cv2.resize(out, (img.shape[1], img.shape[0]))
     return out
@@ -61,14 +54,10 @@ def apply_fisheye(img, debug=False):
     else:
         cv2.namedWindow('undistorted')
         cv2.createTrackbar('k1', 'undistorted', 0, 10000, nothing)
-        # cv2.createTrackbar('k2', 'undistorted', 0, 1000, nothing)
-        # cv2.createTrackbar('fx', 'undistorted', 0, 5500, nothing)
         cv2.createTrackbar('fy', 'undistorted', 0, 5500, nothing)
         while(1):
             # get current positions of four trackbars
             k1 = cv2.getTrackbarPos('k1', 'undistorted')
-            # k2 = cv2.getTrackbarPos('k2', 'undistorted')
-            # fx = cv2.getTrackbarPos('fx', 'undistorted')
             fy = cv2.getTrackbarPos('fy', 'undistorted')
             k1 = k1/10000*20 - 10
             k2 = k1
@@ -127,11 +116,6 @@ def HTRDC(img, steps=50, range_min=-0.25, range_max=0, epsilon=0.0001, iteration
             print(f'k={k:.08f} HT={HT}')
 
     smoothed_HT_vals = gaussian_filter1d(HT_vals, 1.5)
-    # plt.plot(K_range, HT_vals, label='HT')
-    # plt.plot(K_range, HT_vals_focus, label='HT_focused')
-    # plt.legend()
-    # # plt.plot(K_range, smoothed_HT_vals)
-    # plt.show()
     if debug:
         print(f'k={K_range[HT_vals.argmax()]} k_smooth={K_range[smoothed_HT_vals.argmax()]}')
     max_k = K_range[HT_vals_focus.argmax()]
@@ -188,46 +172,18 @@ if __name__ == "__main__":
     for paint in [FISH_EYE, ]:
         img = cv2.imread(paint)
         img = cv2.resize(img, (1280, 720))
-        # img = distort_random(img)
         iv = ImageViewer()
-        # img = cv2.imread("data_test/000090.jpg", cv2.IMREAD_GRAYSCALE)
-        # for e in range(0,100,5):
         mask = generate_mask(img)
         iv.add(mask, cmap='bgr', title='mask')
         
         canvas = np.empty_like(mask, dtype=np.float32)
-        # canvas += _apply_edge_detection(mask, 150, 250)
         canvas += _apply_edge_detection(img, 150, 250)
         canvas = np.clip(canvas, 0, 255)
         iv.add(img, cmap='bgr', title='original')
         iv.add(canvas, cmap='bgr', title='canvas')
 
-        # apply_fisheye(img, True)
-
-        # vals_1 = []
-        # vals_2 = []
-        # pads = range(0, 500, 5)
-        # for pad in pads:
-        #     tmp = canny.copy()
-        #     tmp = _add_padding(tmp, pad)
-        #     # tmp = cv2.resize(tmp, (1280, 720))
-        #     HT = calculate_HT(tmp)
-        #     vals_1.append(HT)
-        #     tmp = without_black_pad(tmp)
-        #     HT = calculate_HT(tmp)
-        #     vals_2.append(HT)
-        #     cv2.imshow('tmp', tmp)
-        #     cv2.waitKey(1)
-        #     print(f'pad={pad} HT={vals_1[-1]} R_HT={vals_2[-1]}')
-        # plt.plot(pads, vals_1)
-        # plt.plot(pads, vals_2)
-        # plt.show()
-
         k = HTRDC(img, steps=50, range_min=-0.25, range_max=0)
         canvas = undistort(img, k1=k)
-        # canvas = undistort(img, k1=-0.38, k2=0.106, fx=872, fy=872)
         cv2.imwrite('data_test/00_calibration_desired.jpg', canvas)
-        # canvas = undistort(img, k1=-0.257, k2=0.008)
         iv.add(canvas, cmap='bgr', title=f'{paint} k={k:.06f}')
         iv.show()
-        # show_me(canvas)
